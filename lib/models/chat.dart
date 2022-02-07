466208bb35cfa1459b62290d35b2dee1d08a3265
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2022-02-08 00:19:52 +0530

    chat model changes

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -1,36 +1,44 @@
-import 'dart:convert';
+// ignore_for_file: constant_identifier_names
 
+enum MessageType {
+  TEXT,
+  IMAGE,
+  VIDEO,
+}
+
+/// [Chat]
+/// Chat Model for defining the chat data
 class Chat {
   final String senderName;
   final String senderid;
   final String message;
+  final MessageType type;
   final DateTime time;
   Chat({
     required this.senderName,
     required this.senderid,
     required this.message,
+    this.type = MessageType.TEXT,
     required this.time,
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'senderName': senderName,
-      'senderid': senderid,
+      'sender_name': senderName,
+      'sender_id': senderid,
       'message': message,
-      'time': time.millisecondsSinceEpoch,
+      'time': time.toIso8601String(),
+      'message_type': type.name,
     };
   }
 
   factory Chat.fromMap(Map<String, dynamic> map) {
     return Chat(
-      senderName: map['senderName'],
-      senderid: map['senderid'],
+      senderName: map['sender_name'],
+      senderid: map['sender_id'],
       message: map['message'],
-      time: DateTime.fromMillisecondsSinceEpoch(map['time']),
+      time: DateTime.parse(map['time']),
+      // type: MessageType.values.,
     );
   }
-
-  String toJson() => json.encode(toMap());
-
-  factory Chat.fromJson(String source) => Chat.fromMap(json.decode(source));
 }
