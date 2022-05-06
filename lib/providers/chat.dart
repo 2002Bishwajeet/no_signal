20c037892b2bbb7c4cda3c1772cd71f006e80a69
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2022-05-06 20:29:37 +0530

    add comments

--- lib/providers/chat.dart
+++ lib/providers/chat.dart
@@ -7,9 +7,19 @@ import '../api/database/chat_services.dart';
 
 /// A Provider to access the [ChatServicesNotifier]
 ///
-final chatProvider =
-    StateNotifierProvider.autoDispose.family<ChatServicesNotifier, List<ChatBubble>,String>(
-        (ref,collectionId) {
+/// A StateNotifierProvider is a Provider that is used to access a StateNotifier
+/// NOTE: autoDispose and family method are also used here.
+/// autoDispose: When the widget is removed from the tree, the provider will be disposed.
+/// It will happen when you close the chat screen with a particular user. If autodispose
+/// didn't called then redudant data will be fetched.
+///
+/// family: This is used to group the providers.
+/// This will be used to take a live paramter when this provider is called
+/// currently we are using it to get the collection id (chat id) so to fetch
+/// the old chats and establish a connection between it.
+final chatProvider = StateNotifierProvider.autoDispose
+    .family<ChatServicesNotifier, List<ChatBubble>, String>(
+        (ref, collectionId) {
   return ChatServicesNotifier(
       client: ref.watch(clientProvider),
       collectionId: collectionId,
